dad1cd2867ae38ce972976abad1b5961252d9ac4
755452800 <steven.chu.csu@gmail.com>
2021-11-15 21:18:55 +0800

    [jewels-and-stones] different ways (hashtable again)

---
+++ jewels-and-stones.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
+
+
+class Solution2(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        d = {}
+        for i in jewels:
+            d[i] = 0
+        for j in stones:
+            if j in d:
+                d[j] += 1
+        return sum(d.values())
+
+
+jewels = "aA"
+stones = "aAAbbbb"
+a = Solution2()
+print(a.numJewelsInStones(jewels, stones))
