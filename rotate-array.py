a474b4ba67512c2d835c487c100c69698a12bdcf
shxhz1 <shxhz1@nottingham.edu.cn>
2021-11-17 14:55:09 +0800

    [rotate-array] to do: finish another two methods

--- rotate-array.py
+++ rotate-array.py
@@ -13,8 +13,17 @@ class Solution:
         nums[:] = nums[- k % len(nums):] + nums[:- k % len(nums)]
 
 
+class Solution2:
+    def rotate(self, nums: List[int], k: int) -> None:
+        pass
+
+
 nums = [1, 2]
 k = 3
 a = Solution()
 a.rotate(nums, k)
+print(nums)
+nums = [1, 2]
+b = Solution2()
+b.rotate(nums, k)
 print(nums)
