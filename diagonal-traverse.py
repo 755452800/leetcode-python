dfe9cad853853f8630a6cf50e22334f1e3efa293
steven <steven.chu.csu@gmail.com>
2021-11-19 12:53:06 +0800

    [spiral-matrix] ugly and not neat way

--- diagonal-traverse.py
+++ diagonal-traverse.py
@@ -4,14 +4,40 @@ link: https://leetcode-cn.com/problems/diagonal-traverse/
 
 from typing import List
 
+
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        row_num = len(mat)
+        row = len(mat)
         col = len(mat[0])
-
-
-
-
+        direction = 1
+        list = []
+        r, c = 0, 0
+        list.append(mat[r][c])
+        while len(list) < row*col:
+            if direction:
+                r = r - 1
+                c = c + 1
+            else:
+                r = r + 1
+                c = c - 1
+            if c < 0 or c >= col or r < 0 or r >= row:
+                if direction:
+                    if c >= col:
+                        r = r + 2
+                        c = c - 1
+                    else:
+                        r = r + 1
+                        c = c
+                else:
+                    if r >= row:
+                        r = r - 1
+                        c = c + 2
+                    else:
+                        r = r
+                        c = c + 1
+                direction = 1 - direction
+            list.append(mat[r][c])
+        return list
 
 
 mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
