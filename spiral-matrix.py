dee747c64e315be8830eac94682bd07fb620f0e3
steven <steven.chu.csu@gmail.com>
2021-11-22 22:31:09 +0800

    [spiral-matrix] bug when matrix has only one line

--- spiral-matrix.py
+++ spiral-matrix.py
@@ -4,5 +4,45 @@ link: https://leetcode-cn.com/problems/spiral-matrix/
 
 from typing import List
 
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        mat = []
+        row = len(matrix)
+        col = len(matrix[0])
+        r = row
+        c = col
+        direction = "right"
+        right, left, up, down = 0, 0, 0, 0
+        vertical = list(zip(*matrix))
+        vertical.reverse()
+        while r > 0 and c > 0:
+            if direction == "right":
+                mat.extend(matrix[right][up:col - down])
+                right += 1
+                r -= 1
+                direction = "down"
+            if direction == "down":
+                mat.extend(vertical[down][right:row - left])
+                down += 1
+                c -= 1
+                direction = "left"
+            if direction == "left":
+                tmp = matrix[row - 1 - left][up:col - down]
+                tmp.reverse()
+                mat.extend(tmp)
+                left += 1
+                r -= 1
+                direction = "up"
+            if direction == "up":
+                tmp = list(list(zip(*matrix))[up][right:row - left])
+                tmp.reverse()
+                mat.extend(tmp)
+                up += 1
+                c -= 1
+        return mat
+
+
+matrix = [[2,3]]
+a = Solution()
+print(a.spiralOrder(matrix))
