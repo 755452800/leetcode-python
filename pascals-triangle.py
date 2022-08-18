ae8086fc27f2fbceac1d8d02be1ef01229bd524d
steven <steven.chu.csu@gmail.com>
2022-08-18 12:41:44 +0800

    numb method (recursion) finished

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -5,7 +5,7 @@ https://leetcode.cn/problems/pascals-triangle/
 from typing import List
 
 
-class Solution:
+class SolutionR:
     def generate(self, numRows: int) -> List[List[int]]:
         result = []
         def fun(n):
@@ -15,16 +15,27 @@ class Solution:
                 return [1, 1]
             if n > 2:
                 R = []
-                R[0] = 1
-                R[-1] = 1
-                for i in range(n - 2):
-                    R[i + 1] = fun(n - 1)[i] + fun(i + 1)
+                for i in range(n):
+                    if i == 0:
+                        R.append(1)
+                    elif i != 0 and i == n-1:
+                        R.append(1)
+                    else:
+                        R.append(fun(n - 1)[i-1] + fun(n - 1)[i])
                 return R
+
         for i in range(numRows):
-            result.extend(fun(i))
+            result.extend([fun(i + 1)])
+        return result
+
+class Solution1:
+    def generate(self, numRows: int) -> List[List[int]]:
+
 
 
 
 numRows = 5
-a = Solution()
+a = SolutionR()
 print(a.generate(numRows))
+
+
